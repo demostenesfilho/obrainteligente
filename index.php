d361c9f7fba67daf238b080df8c785176c47fff5
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2021-03-02 13:14:11 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
 
 <!-- Modal cadastro-->
 <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Criar conta</h5>
