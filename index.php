b7edbc36205a42e354367bc654f68569926847d1
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2021-03-02 13:11:47 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             </ul>
             <div class="d-flex">
                 <button type="button" class="btn btn-primary nav-item" data-bs-toggle="modal" data-bs-target="#exampleModal">Entrar</button><p class="text-light">.</p>
-                <button type="button" class="btn btn-primary">Cadastrar</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">Cadastrar</button>
             </div>
           </div>
         </div>
@@ -78,7 +78,7 @@
       <br>
     </div>
 
-    <!-- Modal -->
+    <!-- Modal login -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
@@ -105,6 +105,85 @@
     </div>
   </div>
 </div>
+
+<!-- Modal cadastro-->
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Criar conta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputNome4" class="form-label">Nome:</label>
+    <input type="text" class="form-control" id="inputNome4" placeholder="Digite seu nome aqui">
+  </div>
+  <div class="col-md-6">
+    <label for="inputSobrenome4" class="form-label">Sobrenome:</label>
+    <input type="text" class="form-control" id="inputSobrenome4" placeholder="Digite seu sobrenome aqui">
+  </div>
+  <div class="col-12">
+    <label for="inputEmail" class="form-label">E-mail:</label>
+    <input type="email" class="form-control" id="inputEmail" placeholder="Digite seu e-mail aqui">
+  </div>
+  <div class="col-12">
+    <label for="inputEndereço" class="form-label">Endereço:</label>
+    <input type="text" class="form-control" id="inputEndereço" placeholder="Rua, Número, Bairro">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCidade" class="form-label">Cidade:</label>
+    <input type="text" class="form-control" id="inputCidade" placeholder="Digite o nome da cidade">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">Estado:</label>
+    <select id="inputState" class="form-select">
+      <option selected>Selecione...</option>
+      <option  value = " Acre " > Acre </option >
+      <option  value = " Alagoas " > Alagoas </option >
+      <option  value = " Amapá " > Amapá </option >
+      <option  value = " Amazonas " > Amazonas </option >
+      <option  value = " Bahia " > Bahia </option >
+      <option  value = " Ceará " > Ceará </option >
+      <option  value = " Distrito Federal " > Distrito Federal </option >
+      <option  value = " Espírito Santo " > Espírito Santo </option >
+      <option  value = " Goiás " > Goiás </option >
+      <option  value = " Maranhão " > Maranhão </option >
+      <option  value = " Mato Grosso " > Mato Grosso </option >
+      <option  value = " Mato Grosso do Sul " > Mato Grosso do Sul </option >
+      <option  value = " Minas Gerais " > Minas Gerais </option >
+      <option  value = " Pará " > Pará </option >
+      <option  value = " Paraíba " > Paraíba </option >
+      <option  value = " Paraná " > Paraná </option >
+      <option  value = " Pernambuco " > Pernambuco </option >
+      <option  value = " Piauí " > Piauí </option >
+      <option  value = " Rio de Janeiro " > Rio de Janeiro </option >
+      <option  value = " Rio Grande do Sul " > Rio Grande do Sul </option >
+      <option  value = " Rio Grande do Norte " > Rio Grande do Norte </option >
+      <option  value = " Rondônia " > Rondônia </option >
+      <option  value = " Roraima " > Roraima </option >
+      <option  value = " Santa Catarina " > Santa Catarina </option >
+      <option  value = " São Paulo " > São Paulo </option >
+      <option  value = " Sergipe " > Sergipe </option >
+      <option  value = " Tocantins " > Tocantins </option >
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Cep:</label>
+    <input type="text" class="form-control" id="inputZip" placeholder="cep">
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Criar conta agora!</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
 
 
